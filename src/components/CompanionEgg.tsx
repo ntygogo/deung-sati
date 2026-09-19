4b9ffec1db8d0d7308f0d3cfa13d85f812b297d9
ntygogo <puksorn.ai@gmail.com>
2026-09-19 21:46:05 +0700

    fix: restore the companion artwork while the 3D model is prepared

--- src/components/CompanionEgg.tsx
+++ src/components/CompanionEgg.tsx
@@ -29,17 +29,7 @@ export function CompanionEgg({ traceCount = 0, size = 320, onPet, isInteracting 
       <span className="living-egg-waterlight" />
       <span className="living-egg-reaction">
         <span className="living-egg-swim">
-          {variant === 'room' ? <span className="embryo-creature" data-testid="egg-embryo">
-            <span className="embryo-tail"><i /></span>
-            <span className="embryo-body">
-              <span className="embryo-gill gill-left"><i /><i /><i /></span>
-              <span className="embryo-gill gill-right"><i /><i /><i /></span>
-              <span className="embryo-face"><i className="embryo-eye left" /><i className="embryo-eye right" /><i className="embryo-mouth" /></span>
-              <i className="embryo-arm arm-left" /><i className="embryo-arm arm-right" />
-              <i className="embryo-leg leg-left" /><i className="embryo-leg leg-right" />
-              <span className="embryo-feeler"><i /></span>
-            </span>
-          </span> : <img className="living-egg-baby" src="/images/companion_dna_base.png" alt="" draggable={false} data-testid="egg-embryo" />}
+          <img className={`living-egg-baby${variant === 'room' ? ' embryo-placeholder' : ''}`} src="/images/companion_dna_base.png" alt="" draggable={false} data-testid="egg-embryo" />
         </span>
       </span>
       {variant === 'room' && <span className="living-egg-habitat" aria-hidden="true">
